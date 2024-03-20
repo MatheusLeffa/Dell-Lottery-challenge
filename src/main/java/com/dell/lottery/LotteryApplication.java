1e5d314480834ff35b8d5a42deb00a96281dca4b
Matt_Leffa <83082324+MatheusLeffa@users.noreply.github.com>
2024-03-20 20:24:13 -0300

    Final Implementation

--- src/main/java/com/dell/lottery/LotteryApplication.java
+++ src/main/java/com/dell/lottery/LotteryApplication.java
@@ -1,10 +1,17 @@
 package com.dell.lottery;
 
 import com.dell.lottery.view.MainMenu;
+import com.formdev.flatlaf.FlatDarculaLaf;
 
+/**
+ * @author Matheus Leffa Hilbert
+ * E-mail: matheus.hilbert@hotmail.com
+ */
 public class LotteryApplication {
 
+    //Inícializa o Menu Principal da aplicação
     public static void main(String[] args) {
+        FlatDarculaLaf.setup();
         MainMenu.startUpMainMenu();
     }
 }
